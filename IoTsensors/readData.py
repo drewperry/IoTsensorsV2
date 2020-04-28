03add02ac28f28774d9b1d5ba91d95a5637335f7
drewperry <49661496+drewperry@users.noreply.github.com>
2020-04-27 23:05:27 -0400

    Python File to Read Data
    This reads from the txt file outputted by the C file

---
+++ IoTsensors/readData.py
@@ -0,0 +1,40 @@
+import re
+
+allTemps = []
+curr_temp = 0
+
+allHeartRate = []
+currHeartRate = 0
+
+allOx = []
+currOx = 0
+
+#Reads line by line
+def read_data():
+    biostats = open(r"biostats.txt", "r")
+    stats = biostats.readlines()
+    for i in stats:
+        if "Obj Temp" in i:
+            obj = re.findall(r'\d+', i)
+            allTemps.append(obj[0])
+            curr_temp = obj[0]
+
+        elif "Heart Rate" in i:
+            rate = re.findall(r'\d+', i)
+            allHeartRate.append(rate[0])
+            currHeartRate = rate[0]
+
+        elif "Oxygen" in i:
+            ox = re.findall(r'\d+', i)
+            allOx.append(ox[0])
+            currOx = ox[0]
+    biostats.close()
+
+def averages():
+    temp_avg = sum(allTemps) / len(allTemps)
+    hr_avg = sum(allHeartRate) / len(allHeartRate)
+    ox_avg = sum(allOx) / len(allOx)
+
+    return [temp_avg, hr_avg, ox_avg]
+
+
